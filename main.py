6c64b9e4760981c61709e8bf50e383c25ad7284c
paragon1234 <vroday@yahoo.com>
2018-04-03 01:35:58 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -175,7 +175,7 @@ def run():
         logits , train_op, cross_entropy_loss = optimize(nn_last_layer , correct_label, learning_rate, num_classes)
 
         # TODO: Train NN using the train_nn function
-        epochs=40
+        epochs=12
         batch_size=10
         train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image,
              correct_label, keep_prob, learning_rate)
